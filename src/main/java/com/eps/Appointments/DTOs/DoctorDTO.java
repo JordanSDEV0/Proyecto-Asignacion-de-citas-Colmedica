ca5dc976708dee9bda392aaef380669bf5dde57a
William <santifon15@gmail.com>
2022-10-04 23:19:01 -0500

    add doctor mapper and DTO

--- src/main/java/com/eps/Appointments/DTOs/DoctorDTO.java
+++ src/main/java/com/eps/Appointments/DTOs/DoctorDTO.java
@@ -2,17 +2,14 @@ package com.eps.Appointments.DTOs;
 
 import java.util.List;
 
-import com.eps.Appointments.persistance.entities.Headquarter;
-import com.eps.Appointments.persistance.entities.Specialization;
-import com.eps.Appointments.persistance.entities.User;
-
 import lombok.Data;
 
 @Data
 public class DoctorDTO {
-    private User user;
-    private Headquarter headquarter;
-    private List<Specialization> specialization;
+
+    private String id;
+    private HeadquarterDTO headquarterDTO;
+    private List<SpecializationDTO> specializationDTOs;
 
 
 }
--- src/main/java/com/eps/Appointments/mappers/DoctorMapper.java
+++ src/main/java/com/eps/Appointments/mappers/DoctorMapper.java
@@ -6,15 +6,14 @@ import org.mapstruct.Mapping;
 import com.eps.Appointments.DTOs.DoctorDTO;
 import com.eps.Appointments.persistance.entities.Doctor;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {HeadquarterMapper.class, SpecializationMapper.class})
 public interface DoctorMapper {
 
-    @Mapping(source = "user.id", target = "id")
-    @Mapping(source = "Headquarter", target = "headquarter")
-    @Mapping(source = "Specialization.description", target = "specialization")
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "headquarter", target = "headquarter")
+    @Mapping(source = "specialization", target = "specialization")
     DoctorDTO toDoctorDTO (Doctor doctor);
 
     @InheritInverseConfiguration
-    @Mapping(target = "user", ignore = true)
     Doctor toDoctor (DoctorDTO doctor);
 }
