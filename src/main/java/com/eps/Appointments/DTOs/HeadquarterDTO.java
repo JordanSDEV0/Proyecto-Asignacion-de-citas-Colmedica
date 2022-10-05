3c8276e06318b03f24028818f5f9bca3db04d6db
William <santifon15@gmail.com>
2022-10-04 23:20:32 -0500

    add headquarter mapper and DTO

--- src/main/java/com/eps/Appointments/DTOs/HeadquarterDTO.java
+++ src/main/java/com/eps/Appointments/DTOs/HeadquarterDTO.java
@@ -1,13 +1,13 @@
 package com.eps.Appointments.DTOs;
 
-import com.eps.Appointments.persistance.entities.City;
 import lombok.Data;
 
 @Data
 public class HeadquarterDTO {
-    	private Integer id;
-    	private City city;
-	    private String name;
-	    private String address;
+
+    private int id;
+    private CityDTO cityDTO;
+	private String name;
+	private String address;
 
 }
--- src/main/java/com/eps/Appointments/mappers/HeadquarterMapper.java
+++ src/main/java/com/eps/Appointments/mappers/HeadquarterMapper.java
@@ -6,20 +6,16 @@ import org.mapstruct.Mapping;
 import com.eps.Appointments.DTOs.HeadquarterDTO;
 import com.eps.Appointments.persistance.entities.Headquarter;
 
-
-
-
-
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = CitiMapper.class)
 public interface HeadquarterMapper {
+
     @Mapping(source = "id", target = "id")
-    @Mapping(source = "City.city", target = "city")
+    @Mapping(source = "city", target = "city")
     @Mapping(source = "name", target = "name")
     @Mapping(source = "address", target = "address")
     HeadquarterDTO toHeadquarterDTO (Headquarter Headquarter);
 
     @InheritInverseConfiguration
-    @Mapping(target = "City", ignore = true)
     Headquarter toHeadquarter (HeadquarterDTO headquarterDTO);
     
 }
