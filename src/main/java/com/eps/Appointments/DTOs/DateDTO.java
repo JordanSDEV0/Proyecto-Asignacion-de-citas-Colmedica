16d2df0d2434a8bf066c7e6c61e25f9a7e8e33a4
William <santifon15@gmail.com>
2022-10-04 23:14:10 -0500

    add date mapper and DTO

--- src/main/java/com/eps/Appointments/DTOs/DateDTO.java
+++ src/main/java/com/eps/Appointments/DTOs/DateDTO.java
@@ -3,21 +3,19 @@ package com.eps.Appointments.DTOs;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import com.eps.Appointments.persistance.entities.DateType;
-import com.eps.Appointments.persistance.entities.Headquarter;
-
 import lombok.Data;
 
 @Data
 public class DateDTO {
-    private Integer id;
-    private DateType dateType;
-	private Headquarter headquarter;
+
+    private int id;
+    private DateTypeDTO dateTypeDTO;
+	private HeadquarterDTO headquarterDTO;
     private String description;	
 	private LocalDateTime initialTime;
-	 	private LocalDateTime finalTime;
-	    private LocalDate date;
-	    private String status;
+	private LocalDateTime finalTime;
+	private LocalDate date;
+	private String status;
 	    
 
 }
--- src/main/java/com/eps/Appointments/mappers/DateMapper.java
+++ src/main/java/com/eps/Appointments/mappers/DateMapper.java
@@ -6,23 +6,20 @@ import org.mapstruct.Mapping;
 import com.eps.Appointments.DTOs.DateDTO;
 import com.eps.Appointments.persistance.entities.Date;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {DateTypeMapper.class, HeadquearterMapper.class})
 public interface DateMapper {
 
     @Mapping(source = "id", target = "id")
-    @Mapping(source = "Datetype", target = "datetype")
-    @Mapping(source = "Headquarter", target = "headquarter")
+    @Mapping(source = "datetype", target = "datetype")
+    @Mapping(source = "headquarter", target = "headquarter")
     @Mapping(source = "description", target = "description")
-    @Mapping(source = "LocalDateTime.initialTime", target = "initialTime")
-    @Mapping(source = "LocalDateTime.finalTime", target = "finalTime")
-    @Mapping(source = "LocalDate.date", target = "date")
+    @Mapping(source = "initialTime", target = "initialTime")
+    @Mapping(source = "finalTime", target = "finalTime")
+    @Mapping(source = "date", target = "date")
     @Mapping(source = "status", target = "status")
-
     DateDTO toDateDto (Date date);
 
     @InheritInverseConfiguration
-    @Mapping(target = "LocalDateTime", ignore = true)
-    @Mapping(target = "LocalDate", ignore = true)
     Date toDate (DateDTO dateDTO);
     
 }
