999518b846a92fb98079e9662a431d1bd96f2671
William <santifon15@gmail.com>
2022-10-05 23:50:10 -0500

    add annotation to admindto

--- src/main/java/com/eps/Appointments/DTOs/AdminDTO.java
+++ src/main/java/com/eps/Appointments/DTOs/AdminDTO.java
@@ -2,10 +2,12 @@ package com.eps.Appointments.DTOs;
 
 import com.eps.Appointments.controllers.AbstractResponse;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data()
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class AdminDTO extends AbstractResponse{
     
