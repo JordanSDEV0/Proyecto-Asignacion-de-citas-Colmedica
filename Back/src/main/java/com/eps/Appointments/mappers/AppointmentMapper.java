5af4066d5c716fc2a520550a3afa9db8071cd535
Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>
2022-10-12 11:42:58 -0500

    Update AppointmentMapper.java
    Signed-off-by: Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/AppointmentMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/AppointmentMapper.java
@@ -13,10 +13,40 @@ import com.eps.Appointments.persistance.entities.Appointment;
 public interface AppointmentMapper {
 
     @Mapping(source = "id", target = "id")
-    @Mapping(source = "doctor", target = "doctor")
-    @Mapping(source = "patient", target = "patient")
-    @Mapping(source = "date", target = "date")
+    @Mapping(source = "doctor", target = "doctorId")
+    @Mapping(source = "patient", target = "patientId")
+    @Mapping(source = "date", target = "dateId")
     AppointmentDTO toAppointmentDTO (Appointment appointment);
+    
+    default Doctor mapDoctor(String id){
+        Doctor d= new Doctor();
+        d.setId(id);
+        return d;
+    }
+    
+    default Patient mapPatient(String id){
+        Patient d= new Patient();
+        d.setId(id);
+        return d;
+    }
+    
+    default Date mapDate(String id){
+        Date d= new Date();
+        d.setId(id);
+        return d;
+    }
+    
+    default String mapDoctor(Doctor doctor){
+        return doctor.getId();
+    }
+    
+    default String mapPatient(Patient doctor){
+        return doctor.getId();
+    }
+    
+    default int mapDate(Date doctor){
+        return doctor.getId();
+    }
 
     @InheritInverseConfiguration
     @Mapping(target = "doctor", ignore = true)
