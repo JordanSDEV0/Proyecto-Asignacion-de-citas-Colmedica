5788ce953e4737d01635c9d44149e83552d1783a
Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>
2022-10-13 08:38:14 -0500

    Update AppointmentService.java
    Signed-off-by: Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/services/AppointmentService.java
+++ Back/src/main/java/com/eps/Appointments/services/AppointmentService.java
@@ -6,43 +6,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.eps.Appointments.DTOs.AppointmentDTO;
+import com.eps.Appointments.DTOs.SchedulingDTO;
+import com.eps.Appointments.DTOs.DateDTO;
+import com.eps.Appointments.mappers.SchedulingMapper;
 import com.eps.Appointments.mappers.AppointmentMapper;
+import com.eps.Appointments.mappers.DateMapper;
 import com.eps.Appointments.persistance.entities.Appointment;
+import com.eps.Appointments.persistance.entities.Date;
 import com.eps.Appointments.persistance.repositories.AppointmentRepository;
+import com.eps.Appointments.persistance.repositories.DateRepository;
 
 @Service
-public class AppointmentService{
+public class SchedulingService{
 
-    @Autowired
-    private AppointmentRepository appointmentRepository;
-    @Autowired
-    private AppointmentMapper appointmentMapper;
+  @Autowired
+  private SchedulingMapper schedulingMapper;
+  @Autowired
+  private AppointmentService appointmentService;
+  @Autowired
+  private DateService dateService;
 
-    @Transactional
-    public AppointmentDTO create(AppointmentDTO appointmentDTO) throws IllegalArgumentException{
-        System.out.println(appointmentMapper.toAppointment(appointmentDTO));
-        return appointmentMapper.toAppointmentDTO(appointmentRepository.save(appointmentMapper.toAppointment(appointmentDTO)));
-    }
-    
-    public List<AppointmentDTO> getAll(){
-        List<Appointment> appointments= (List<Appointment>) appointmentRepository.findAll();
-        return appointmentMapper.toAppointmentDTOs(appointments);
-    }
-
-    @Transactional
-    public AppointmentDTO updateAppointment(AppointmentDTO appointmentDTO){
-        if(getById(appointmentDTO.getId()) != null){
-            Appointment updatedAppointment = appointmentMapper.toAppointment(appointmentDTO);
-            return appointmentMapper.toAppointmentDTO(appointmentRepository.save(updatedAppointment));
-        }
-        return null;
-    }
-
-    public AppointmentDTO getById(int id){
-        return appointmentMapper.toAppointmentDTO(appointmentRepository.findById(id).map(ocupation -> {
-            return ocupation;
-        }).orElseGet(null));
-    }
-
+  @Transactional
+  private SchedulingDTO create(SchedulingDTO scheduling){
+      DateDTO dateDTO= dateService.create(schedulingMapper.toDateDTO(sheduling));
+      if(dateDTO != null){
+          AppointmentDTO appointmentDTO= appointmentService.create(shedulingMapper.toAppointmentDTO(scheduling));
+          if(appointmentDTO != null){
+            return scheduling;
+          }
+          return null;
+      }
+      return null;    
+  }
+  
 }
