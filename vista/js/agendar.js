91071b98b9731ff43aaa8bcd51bf64165606507f
Fonseca Mora <william.fonseca-mora@capgemini.com>
2022-10-13 16:20:24 -0500

    fetch scheduling

--- vista/js/agendar.js
+++ vista/js/agendar.js
@@ -3,6 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     let form= document.getElementById("form")
     form.addEventListener("submit", (event) => {
         event.preventDefault();
+        data= {
+            "dateTypeId": 1,
+            "headquarterId": 1,
+            "description": "example",
+            "initialTime": form.elements['initial_time'].value,
+            "finalTime": form.elements['final_time'].value,
+            "date": form.elements['date'].value,
+            "status": "example",
+            "doctorId": "doctor",
+            "patientId": "patient"            
+        }
+        //console.log(data);
+        createAppointment(baseUrl, data);
     });
 });
 
