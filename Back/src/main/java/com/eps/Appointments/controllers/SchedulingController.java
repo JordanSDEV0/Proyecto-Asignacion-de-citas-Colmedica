2f02dd6a7609ca612ef3a6b6784bbf3b72e4f92e
Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>
2022-10-13 08:39:20 -0500

    Create SchedulingController.java
    Signed-off-by: Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>

---
+++ Back/src/main/java/com/eps/Appointments/controllers/SchedulingController.java
@@ -0,0 +1,68 @@
+package com.eps.Appointments.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eps.Appointments.DTOs.SchedulingDTO;
+import com.eps.Appointments.DTOs.ErrorDTO;
+import com.eps.Appointments.services.SchedulingService;
+
+@RestController
+@RequestMapping("/scheduling")
+@CrossOrigin
+public class SchedulingController {
+
+    @Autowired
+    private SchedulingService schedulingService;
+
+    @PostMapping
+    private ResponseEntity<? extends Object> create(@RequestBody SchedulingDTO scheduling){
+        try {
+          SchedulingDTO newScheduling= schedulingService.create(scheduling);
+          if(scheduling != null){
+            return new ResponseEntity<SchedulingDTO>(scheduling, HttpStatus.CREATED);
+          }
+          return new ResponseEntity<ErrorDTO>(new ErrorDTO("Cita no creada"), HttpStatus.ACCEPTED);
+        } catch (IllegalArgumentException illegalArgumentException){
+            System.out.println(illegalArgumentException.getCause());
+            return new ResponseEntity<ErrorDTO>(new ErrorDTO(illegalArgumentException.getMessage()), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+            return new ResponseEntity<ErrorDTO>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<? extends Object> getAll(){
+        try{
+            return new ResponseEntity<>(null, HttpStatus.OK);
+	    } catch(IllegalArgumentException illegalArgumentException){
+	        System.out.println(illegalArgumentException.getCause());
+	        return new ResponseEntity<>(new ErrorDTO(illegalArgumentException.getMessage()), HttpStatus.NOT_FOUND);
+        } catch(Exception e){
+	        System.out.println(e.getCause());
+            return new ResponseEntity<>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{id}")
+        public ResponseEntity<? extends Object> getById(@PathVariable("id") Integer id){
+        try{
+            return new ResponseEntity<AppointmentDTO>(null, HttpStatus.OK);
+	} catch(IllegalArgumentException illegalArgumentException){
+	    System.out.println(illegalArgumentException.getCause());
+	    return new ResponseEntity<ErrorDTO>(new ErrorDTO(illegalArgumentException.getMessage()), HttpStatus.NOT_FOUND);
+    } catch(Exception e){
+	    System.out.println(e.getCause());
+        return new ResponseEntity<ErrorDTO>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+    }
+}
