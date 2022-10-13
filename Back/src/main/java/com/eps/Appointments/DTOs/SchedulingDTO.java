9c0b50e50011dc8a570ebd574076bc9b6c12cb0b
Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>
2022-10-13 08:01:26 -0500

    Create SchedulingDTO.java
    Signed-off-by: Wiliamfm <65041178+Wiliamfm@users.noreply.github.com>

---
+++ Back/src/main/java/com/eps/Appointments/DTOs/SchedulingDTO.java
@@ -0,0 +1,24 @@
+package com.eps.Appointments.DTOs;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SchedulingDTO {
+  
+  private int id;
+  private int dateTypeId;
+	private int headquarterId;
+  private String description;	
+	private LocalDateTime initialTime;
+	private LocalDateTime finalTime;
+	private LocalDate date;
+	private String status;
+  private String doctorId;
+  private String patientId;
+
+}
