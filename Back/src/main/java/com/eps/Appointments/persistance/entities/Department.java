5ea031389d00d9236f4b2624f3cdf6fa424d345f
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:21:28 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Department.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Department.java
@@ -1,14 +1,24 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Se crea la clase Departament para saber en donde se encuentra ubicado
+ * The Department class is created to know where it is located
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
  * @author:Sofware Bosque S.A
  */
 @Entity
@@ -16,11 +26,17 @@ import lombok.NoArgsConstructor;
 @Data
 public class Department {
 /**
- * Creamos sus atributos con su respectivo id y department con sus respectivos tipos que son private
+ * We create its attributes with their respective id and department with their respective types that are private
  */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
+    /** 
+    * Private attribute of String type with name departament
+    **/
     private String department;
     
 }
