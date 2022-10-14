058f02dd66694c55b70162076f762ab4f2d87007
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-13 22:56:35 -0500

    Update Access.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Access.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Access.java
@@ -12,12 +12,17 @@ import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Se tiene la clase Access la cual va a servir para acceder a la eps
+ * @author:Sofware Bosque S.A
+ */
 @Entity
 @NoArgsConstructor
 @Data
 public class Access {
-
+/**
+ *Se define los atributos con su respectivo tipo para poder acceder tranquilamente a la eps
+ */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
