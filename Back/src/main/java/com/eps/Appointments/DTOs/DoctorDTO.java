247752ac76d483fd2416e315d11db805fb9cf5fa
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:21:37 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/DoctorDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/DoctorDTO.java
@@ -1,17 +1,39 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of java
+*/
 import java.util.List;
-
+/**
+* Imports of lombok
+*/
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+/**
+* Public class for dto doctor
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class DoctorDTO {
-
+/**
+    * Private attribute of type String of name id
+    **/
     private String id;
+    /**
+    * Private attribute of type String of name password
+    **/
     private String password;
+    /**
+    * Private attribute of type int of name headquarterId
+    **/
     private int headquarterId;
+    /**
+    * Private attribute of type list SpecializationDTO of name specializationList
+    **/
     private List<SpecializationDTO> specializationList;
 
 }
