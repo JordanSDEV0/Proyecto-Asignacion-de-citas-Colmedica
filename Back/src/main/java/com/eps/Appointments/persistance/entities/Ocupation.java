da4b2f53207055c91921245206d00338c81a22d1
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-13 22:13:40 -0500

    Update Ocupation.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Ocupation.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Ocupation.java
@@ -7,12 +7,19 @@ import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+  *  Se tiene la clase Ocupation la cual asiganara cada una de esta dependiendo de sus caracteristicas
+   * @author:Sofware Bosque S.A
+ */
 @Entity
 @NoArgsConstructor
 @Data
-public class Ocupation {
 
+public class Ocupation {
+/**
+ * 
+ * Se define los atributos para poder generar la ocupacion dependiendo de su ID y Ocupation
+  */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
