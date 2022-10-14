7a5fe3263b058686f8ac8b33cea8bb9fc7803025
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-13 22:17:17 -0500

    Update IdType.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/IdType.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/IdType.java
@@ -7,12 +7,17 @@ import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Se crea la clase IdType para tener cada tipo de ID
+ * @author:Sofware Bosque S.A
+ */
 @Entity
 @NoArgsConstructor
 @Data
 public class IdType {
-
+/**
+ * Se definen sus atributos que son id y type con sus respectivos tipos que son private 
+ */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
