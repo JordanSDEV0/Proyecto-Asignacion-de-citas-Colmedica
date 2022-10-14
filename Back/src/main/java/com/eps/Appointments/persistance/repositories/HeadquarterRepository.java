76ccf8e1584da36dd887833659413dc6c2d0e2b6
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:00:01 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/HeadquarterRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/HeadquarterRepository.java
@@ -1,12 +1,23 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports java.util.Optional
+*/
 import java.util.Optional;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.Headquarter;
-
+/**
+* interface with name HeadquarterRepository which comes from CrudRepository<Headquarter, Integer>
+*/
 @Repository
 public interface HeadquarterRepository extends CrudRepository<Headquarter, Integer>{
 
