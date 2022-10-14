b8c8a0550be0df17342b9989220a6ca49e218a49
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:49:01 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/CertificateMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/CertificateMapper.java
@@ -1,12 +1,22 @@
+/**
+* Package with which the persistence of the mappers is accessed
+**/
 package com.eps.Appointments.mappers;
-
+/**
+* Imports of mapstruct
+*/
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.CertificateDTO;
 import com.eps.Appointments.persistance.entities.Certificate;
-
+/**
+* public name interface CertificateMapper
+* @mapper geographic information system (GIS)
+**/
 @Mapper(componentModel = "spring", uses= PatientMapper.class)
 public interface CertificateMapper {
     
