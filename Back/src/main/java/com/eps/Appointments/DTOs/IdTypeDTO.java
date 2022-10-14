7c04d2d98089f4830491008f1d6c0463a7e7e105
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:23:23 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/IdTypeDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/IdTypeDTO.java
@@ -1,13 +1,27 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of lombok
+*/
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+/**
+* Public class for dto access
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class IdTypeDTO {
-
+/**
+    * Private attribute of type int of name id
+    **/
 	private int id;
+	/**
+    * Private attribute of type String of name type
+    **/
     private String type;
     
 }
