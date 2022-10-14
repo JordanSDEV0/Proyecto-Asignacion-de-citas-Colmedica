c21d5ad109c339189548b6b7bbf9008bbf0f6bfc
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:30:53 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/IdType.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/IdType.java
@@ -1,14 +1,24 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Se crea la clase IdType para tener cada tipo de ID
+ * IdType class is created to hold each type of ID
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
  * @author:Sofware Bosque S.A
  */
 @Entity
@@ -16,11 +26,17 @@ import lombok.NoArgsConstructor;
 @Data
 public class IdType {
 /**
- * Se definen sus atributos que son id y type con sus respectivos tipos que son private 
+ * Its attributes are defined, which are id and type, with their respective types, which are private.
  */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
+    /** 
+    * Private attribute of String type with name type
+    **/
     private String type;
     
 }
