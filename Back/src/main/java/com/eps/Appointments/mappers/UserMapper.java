ea94e24ce698ce8e1714d60b389f669b566d7fc9
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:59:36 -0500

    Docmentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/UserMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/UserMapper.java
@@ -1,21 +1,35 @@
+/**
+* Package with which the persistence of the mappers is accessed
+**/
 package com.eps.Appointments.mappers;
-
+/**
+* Imports of java
+*/
 import java.util.List;
-
+/**
+* Imports of mapstruct
+*/
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.UserDTO;
 import com.eps.Appointments.persistance.entities.User;
-
+/**
+* public name interface UserMapper
+* @mapper geographic information system (GIS)
+**/
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     @Mapping(source = "id", target = "id")
     @Mapping(source = "password", target = "password")
     UserDTO toUserDTO (User user);
-
+/**
+* Advises the code generator to apply all the Mapping s from an inverse mapping method to the annotated method as well
+**/
     @InheritInverseConfiguration
     User toUser (UserDTO userDTO);
 
