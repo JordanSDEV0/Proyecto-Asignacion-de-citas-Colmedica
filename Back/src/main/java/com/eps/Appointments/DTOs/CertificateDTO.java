0dda85f9abfe8a4c2fd75d1eee41ceada9e280eb
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:07:10 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/CertificateDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/CertificateDTO.java
@@ -1,16 +1,39 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of date
+*/
 import lombok.Data;
-
+/**
+* Imports of lombok
+*/
 import lombok.AllArgsConstructor;
-
+/**
+* Public class for dto certificated
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class CertificateDTO {
-
+    
+    /**
+    * Private attribute of type int of name id
+    **/
     private int id;
+    /**
+    * Private attribute of type PatientDTO of name patient
+    **/
     private PatientDTO patient;
+    /**
+    * Private attribute of type String of name rute
+    **/
     private String rute;
+    /**
+    * Private attribute of type String of name type
+    **/
     private String type;
 
 }
