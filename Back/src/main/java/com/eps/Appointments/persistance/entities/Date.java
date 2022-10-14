a4e082547897cea4a6bd50cf5626e30ce9fcbdd9
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:17:41 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Date.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Date.java
@@ -1,8 +1,15 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports of time
+*/
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,11 +17,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Se crea la clase date la cual tendra todas las caracteristicas de esta misma
+ * The date class is created which will have all the characteristics of it
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
  * @author:Sofware Bosque S.A
  */
 @Entity
@@ -22,23 +34,47 @@ import lombok.NoArgsConstructor;
 @Data
 public class Date {
 /**
- * Se definen los atributos de los datos y se llaman algunos otros y cada uno es de tipo private
- */
+ * The attributes of the data are defined and some others are called and each one is of type private
+ **/
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "date_type", nullable = false)
+    /** 
+    * Private attribute of DateType type with name dateType
+    **/
     private DateType dateType;
     @ManyToOne
     @JoinColumn(nullable = false)
+    /** 
+    * Private attribute of Headquarter type with name headquarter
+    **/
     private Headquarter headquarter;
+    /** 
+    * Private attribute of String type with name description
+    **/
     private String description;
     @Column(name = "initial_hour", nullable = false)
+    /** 
+    * Private attribute of LocalDateTime type with name initialTime
+    **/
     private LocalDateTime initialTime;
     @Column(name = "final_hour")
+    /** 
+    * Private attribute of LocalDateTime type with name finalTime
+    **/
     private LocalDateTime finalTime;
+    /** 
+    * Private attribute of LocalDate type with name date
+    **/
     private LocalDate date;
+    /** 
+    * Private attribute of String type with name status
+    **/
     private String status;
     
 }
