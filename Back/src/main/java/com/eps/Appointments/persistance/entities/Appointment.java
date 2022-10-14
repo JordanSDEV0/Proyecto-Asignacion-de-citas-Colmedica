1760b34d249160d3c85ccbef03e2a22d6e3cdc2c
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:10:40 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Appointment.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Appointment.java
@@ -1,16 +1,26 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
  * Se tiene la clase Appointment la cual son citas 
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
  * @author:Sofware Bosque S.A
  */
 @Entity
@@ -18,19 +28,31 @@ import lombok.NoArgsConstructor;
 @Data
 public class Appointment {
 /**
- * Esta clase esta definida por el doctor, paciente y dato para poder saber todo acerca sobre la cita
+ * This class is defined by the doctor, patient and data to be able to know everything about the appointment
  */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
     @ManyToOne
     @JoinColumn(nullable = false)
+    /** 
+    * Private attribute of doctor type with name Doctor
+    **/
     private Doctor doctor;
     @ManyToOne
     @JoinColumn(nullable = false)
+    /** 
+    * Private attribute of Patient type with name patient
+    **/
     private Patient patient;
     @ManyToOne
     @JoinColumn(nullable = false)
+    /** 
+    * Private attribute of Date type with name date
+    **/
     private Date date;
     
 }
