2a39b5c914a940a6ad089c38c2b043bf22806fbe
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:01:11 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/PatientRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/PatientRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.Patient;
-
+/**
+* interface with name PatientRepository which comes from CrudRepository<Patient, Integer>
+*/
 @Repository
 public interface PatientRepository extends CrudRepository<Patient, String>{
 
