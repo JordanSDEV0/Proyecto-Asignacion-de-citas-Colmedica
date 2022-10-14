a6cece0bdc2e26a5d943a60220b6acfaa855b420
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 11:55:11 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/DateRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/DateRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.Date;
-
+/**
+* interface with name DateRepository which comes from CrudRepository<Date, Integer>
+*/
 @Repository
 public interface DateRepository extends CrudRepository<Date, Integer> {
 
