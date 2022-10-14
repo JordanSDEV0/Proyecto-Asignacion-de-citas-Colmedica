7952ef03ff87d1f459048c999697e021d74d7891
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:09:22 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/SpecializationDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/SpecializationDTO.java
@@ -1,13 +1,26 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of lombok
+*/
 import lombok.Data;
 import lombok.AllArgsConstructor;
-
+/**
+* Public class for dto specilization
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class SpecializationDTO {
-
-	  	private Integer id;
+/**
+    * Private attribute of type int of name id
+    **/
+	  	private Integer id;/**
+    * Private attribute of type String of name description
+    **/
 	    private String description;
 
 }
