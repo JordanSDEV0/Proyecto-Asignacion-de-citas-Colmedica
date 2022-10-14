907cdea4e53fa9ff6d659cbdfd818645adac6b55
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:22:28 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/ErrorDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/ErrorDTO.java
@@ -1,12 +1,23 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of lombok
+*/
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+/**
+* Public class for dto error
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class ErrorDTO {
-
+/**
+    * Private attribute of type String of name message
+    **/
     private String message;
     
 }
