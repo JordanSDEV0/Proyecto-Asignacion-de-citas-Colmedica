277585a41df245c855e5d297d30ccd586263fb28
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:53:37 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/HeadquarterMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/HeadquarterMapper.java
@@ -1,14 +1,26 @@
+/**
+* Package with which the persistence of the mappers is accessed
+**/
 package com.eps.Appointments.mappers;
-
+/**
+* Imports of java
+*/
 import java.util.List;
-
+/**
+* Imports of mapstruct
+*/
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.HeadquarterDTO;
 import com.eps.Appointments.persistance.entities.Headquarter;
-
+/**
+* public name interface HeadquarterMapper
+* @mapper geographic information system (GIS)
+**/
 @Mapper(componentModel = "spring", uses = CityMapper.class)
 public interface HeadquarterMapper {
 
