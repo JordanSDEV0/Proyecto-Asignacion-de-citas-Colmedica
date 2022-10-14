be7e036b91ec02f1bb93cc140a12d25634e92667
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:40:42 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/User.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/User.java
@@ -1,15 +1,25 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Desde esta clase se podra acceder a los user
- * @author: 
+ * From this class you can access the user
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
+ * @author:Software Bosque S.A
  *
  */
 @Entity(name = "eps_user")
@@ -17,13 +27,19 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 /**
- *Esta clase sera user y estara con atributos de tipo privado con su respectivo Id y password para poder acceder
+ * This class will be user and will have attributes of a private type with its respective Id and password to be able to access
  */
 public class User {
 
     @Id
+    /** 
+    * Private attribute of String type with name id
+    **/
     private String id;
     @Column(nullable = false)
+    /** 
+    * Private attribute of String type with name password
+    **/
     private String password;
     
 }
