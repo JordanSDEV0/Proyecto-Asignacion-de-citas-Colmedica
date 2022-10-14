bc36a16381f2ab0df680ea404b61cac8fbaed4af
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-13 22:42:40 -0500

    Update DateType.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/DateType.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/DateType.java
@@ -7,12 +7,17 @@ import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Se crea la clase DateType
+ * @author:Sofware Bosque S.A
+ */
 @Entity(name = "date_type")
 @NoArgsConstructor
 @Data
 public class DateType {
-
+/**
+ * Se generan los atributos como id y description con sus respectivo tipo
+ */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
