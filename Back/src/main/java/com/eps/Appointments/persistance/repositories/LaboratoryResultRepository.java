aa48c5e28ddaa0072d252a0a4b8137b668c941d1
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:01:50 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/LaboratoryResultRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/LaboratoryResultRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.LaboratoryResult;
-
+/**
+* interface with name LaboratoryResultRepository which comes from CrudRepository<LaboratoryResult, Integer>
+*/
 @Repository
 public interface LaboratoryResultRepository extends CrudRepository<LaboratoryResult, Integer>{
 
