9d8e0e763ad274ebcc806aaf95be803292d5c1d9
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:19:56 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/DateType.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/DateType.java
@@ -1,14 +1,24 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Se crea la clase DateType
+ * DateType class is created
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data 
  * @author:Sofware Bosque S.A
  */
 @Entity(name = "date_type")
@@ -16,12 +26,17 @@ import lombok.NoArgsConstructor;
 @Data
 public class DateType {
 /**
- * Se generan los atributos como id y description con sus respectivo tipo
+ * Attributes are generated as id and description with their respective type
  */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+     /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
-
+     /** 
+    * Private attribute of numeric type with name id
+    **/
     private String description;
     
 }
