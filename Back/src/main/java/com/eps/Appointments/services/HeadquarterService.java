4d1c4af46eb0f3495642f79202487994a5eef974
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:46:39 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/services/HeadquarterService.java
+++ Back/src/main/java/com/eps/Appointments/services/HeadquarterService.java
@@ -1,29 +1,52 @@
+/**
+* Package with which the services of the appointments access
+**/
 package com.eps.Appointments.services;
-
+/**
+* Imports of java util
+*/
 import java.util.List;
-
+/**
+* Imports of springframework
+*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.HeadquarterDTO;
 import com.eps.Appointments.mappers.HeadquarterMapper;
 import com.eps.Appointments.persistance.entities.Headquarter;
 import com.eps.Appointments.persistance.repositories.HeadquarterRepository;
-
+/**
+* Creation of the public class HeadquarterService
+* @Service It is used to mark the class as a service provider 
+**/
 @Service
 public class HeadquarterService {
-
-    @Autowired
+    /**
+    * annotation that allows to inject some dependencies with others inside Spring
+    **/
+    @Autowired/**
+    * Private attribute of type HeadquarterRepository of name headquarterRepository
+    **/
     private HeadquarterRepository headquarterRepository;
+    /**
+    * Private attribute of type HeadquarterMapper of name headquarterMapper
+    **/
     @Autowired
     private HeadquarterMapper headquarterMapper;
-
+    /**
+    * creation of the class HeadquarterDTO getById with name attributes
+    **/
     public HeadquarterDTO getByName(String name){
         return headquarterMapper.toHeadquarterDTO(headquarterRepository.findByName(name).map(h -> {
             return h;
         }).orElse(null));
     }
-
+    /**
+    * creation of the class List<HeadquarterDTO> getAll
+    **/
     public List<HeadquarterDTO> getAll(){
         return headquarterMapper.toHeadquarterDTOs((List<Headquarter>) headquarterRepository.findAll());
     }
