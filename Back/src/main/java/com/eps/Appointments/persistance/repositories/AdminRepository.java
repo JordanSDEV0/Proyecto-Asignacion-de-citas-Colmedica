393fdd1e80ed3101e2bc674fdbdaa390de52d971
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 11:50:08 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/AdminRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/AdminRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.Admin;
-
+/**
+* interface with name AdminRepository which comes from CrudRepository<Admin, Stringr>
+*/
 @Repository
 public interface AdminRepository extends CrudRepository<Admin, String>{
     
