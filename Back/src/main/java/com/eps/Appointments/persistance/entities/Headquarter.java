d311304a94210fe078c92205a771c145c3632e63
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:28:47 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Headquarter.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Headquarter.java
@@ -1,5 +1,10 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,11 +12,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Se crea la clase Headquarter la cual nos sirve para las cedes
+ * The Headquarter class is created which serves us for the cedes
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
  * @author:Sofware Bosque S.A
   */
 @Entity
@@ -19,16 +29,28 @@ import lombok.NoArgsConstructor;
 @Data
 public class Headquarter {
 /**
- * Estos atributos nos serviran para saber si hay una cede en una ciudad y si llega a ser asi tambien le colocamos el nombre y dirreccion
+ * These attributes will help us to know if there is a cede in a city and if it becomes so, we will also put the name and address
   */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+   /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "city", nullable = false)
+    /** 
+    * Private attribute of City type with name city
+    **/
     private City city;
     @Column(unique = true)
+    /** 
+    * Private attribute of String type with name name
+    **/
     private String name;
+    /** 
+    * Private attribute of String type with name address
+    **/
     private String address;
     
 }
