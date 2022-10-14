0c2d0fd22c704b88a87a00d029b8895cba9c1dc5
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:18:31 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/DateDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/DateDTO.java
@@ -1,22 +1,56 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of time
+*/
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+/**
+* Imports of lombok
+*/
 import lombok.Data;
 import lombok.AllArgsConstructor;
-
+/**
+* Public class for dto date
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class DateDTO {
-
+/**
+    * Private attribute of type int of name id
+    **/
     private int id;
+	/**
+    * Private attribute of type int of name dateTypeId
+    **/
     private int dateTypeId;
+	/**
+    * Private attribute of type int of name headquarterId
+    **/
 	private int headquarterId;
+	/**
+    * Private attribute of type String of name description
+    **/
     private String description;	
+	/**
+    * Private attribute of type LocalDateTime of name initialTime
+    **/
 	private LocalDateTime initialTime;
+	/**
+    * Private attribute of type LocalDateTime of name finalTime
+    **/
 	private LocalDateTime finalTime;
+	/**
+    * Private attribute of type LocalDate of name date
+    **/
 	private LocalDate date;
+	/**
+    * Private attribute of type String of name status
+    **/
 	private String status;
 	    
 }
