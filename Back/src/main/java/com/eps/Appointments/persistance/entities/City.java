5e73c6e1f3432a339edec06d5d9af3cf679cb26d
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-13 22:46:28 -0500

    Update City.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/City.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/City.java
@@ -9,12 +9,17 @@ import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Se define la clase city para saber las ubicaciones de los centros
+ * @author:Sofware Bosque S.A
+ */
 @Entity
 @NoArgsConstructor
 @Data
 public class City {
-
+/**
+ * Se define las ciudades con su departamento id y ciudad esto quiere decir que nos mostrara el departamiento y ciudad en donde este el centro 
+ */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
