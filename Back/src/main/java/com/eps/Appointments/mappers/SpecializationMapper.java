ae4bd026df45bb120e2956b47412414b04fd2b9a
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:58:36 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/SpecializationMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/SpecializationMapper.java
@@ -1,12 +1,22 @@
+/**
+* Package with which the persistence of the mappers is accessed
+**/
 package com.eps.Appointments.mappers;
-
+/**
+* Imports of mapstruct
+*/
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.SpecializationDTO;
 import com.eps.Appointments.persistance.entities.Specialization;
-
+/**
+* public name interface SpecializationMapper
+* @mapper geographic information system (GIS)
+**/
 @Mapper(componentModel = "spring")
 public interface SpecializationMapper {
     
