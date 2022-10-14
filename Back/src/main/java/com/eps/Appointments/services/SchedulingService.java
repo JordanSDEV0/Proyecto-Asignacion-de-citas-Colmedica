70a8555923e2a25c67e3864710363da37143a044
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:53:46 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/services/SchedulingService.java
+++ Back/src/main/java/com/eps/Appointments/services/SchedulingService.java
@@ -1,24 +1,48 @@
+/**
+* Package with which the services of the appointments access
+**/
 package com.eps.Appointments.services;
-
+/**
+* Imports of springframework
+*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.SchedulingDTO;
 import com.eps.Appointments.DTOs.AppointmentDTO;
 import com.eps.Appointments.DTOs.DateDTO;
 import com.eps.Appointments.mappers.SchedulingMapper;
-
+/**
+* Creation of the public class SchedulingService
+* @Service It is used to mark the class as a service provider 
+**/
 @Service
 public class SchedulingService{
-
+  /**
+    * annotation that allows to inject some dependencies with others inside Spring
+    **/
   @Autowired
+  /**
+    * Private attribute of type SchedulingMapper of name schedulingMapper
+    **/
   private SchedulingMapper schedulingMapper;
   @Autowired
+  /**
+    * Private attribute of type AppointmentService of name appointmentService
+    **/
   private AppointmentService appointmentService;
   @Autowired
+  /**
+    * Private attribute of type DateService of name dateService
+    **/
   private DateService dateService;
-
+  /**
+    * class creation SchedulingDTO with attributes SchedulingDTO 
+    * @Transactional annotation is the metadata that specifies the semantics of the transactions on a method
+    **/
   @Transactional
   public SchedulingDTO create(SchedulingDTO scheduling){
       DateDTO dateDTO= dateService.create(schedulingMapper.toDateDto(scheduling));
