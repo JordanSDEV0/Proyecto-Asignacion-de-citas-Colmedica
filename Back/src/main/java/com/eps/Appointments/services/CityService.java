fe66a62e1e581e67c661c99d194eada9094d4861
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:37:08 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/services/CityService.java
+++ Back/src/main/java/com/eps/Appointments/services/CityService.java
@@ -1,17 +1,34 @@
+/**
+* Package with which the services of the appointments access
+**/
 package com.eps.Appointments.services;
-
+/**
+* Imports of springframework
+*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.persistance.entities.City;
 import com.eps.Appointments.persistance.repositories.CityRepository;
-
+/**
+* Creation of the public class CityService
+* @Service It is used to mark the class as a service provider 
+**/
 @Service
 public class CityService {
-
+/**
+    * annotation that allows to inject some dependencies with others inside Spring
+    **/
     @Autowired
+    /**
+    * Private attribute of type CityRepository of name cityRepository
+    **/
     private CityRepository cityRepository;
-
+/**
+    * creation of the class City getUser with id attributes
+    **/
     public City getUser(Integer id){
         return cityRepository.findById(id).map(city -> {
             return city;
