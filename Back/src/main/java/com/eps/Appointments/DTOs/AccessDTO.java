7c7679e3042f849ac55392ef2eece4ee98318777
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:03:13 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/AccessDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/AccessDTO.java
@@ -1,16 +1,35 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of time
+*/
 import java.time.LocalDateTime;
-
+/**
+* Imports of lombok
+*/
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+/**
+* Public class for dto access
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class AccessDTO{
-
+    /**
+    * Private attribute of type int of name id
+    **/
     private int id;
+    /**
+    * Private attribute of type UserDTO of name user
+    **/
     private UserDTO user;
+    /**
+    * Private attribute of type LocalDateTime of name accessTime
+    **/
     private LocalDateTime accessTime;
 
 }
