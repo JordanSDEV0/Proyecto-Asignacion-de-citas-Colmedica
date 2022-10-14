3559583d39d4bd63a9cb2a81741481498cb2bb86
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:52:10 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/DepartmentMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/DepartmentMapper.java
@@ -1,12 +1,22 @@
+/**
+* Package with which the persistence of the mappers is accessed
+**/
 package com.eps.Appointments.mappers;
-
+/**
+* Imports of mapstruct
+*/
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.DepartmentDTO;
 import com.eps.Appointments.persistance.entities.Department;
-
+/**
+* public name interface DepartmentMapper
+* @mapper geographic information system (GIS)
+**/
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
 
