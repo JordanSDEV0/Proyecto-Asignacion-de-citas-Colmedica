45f4a3c19d3cc9c09a5f724cc3424cf2268249aa
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:13:12 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Certificate.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Certificate.java
@@ -1,16 +1,26 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Se define la clase Certificate para darle los certificado a los usuarios de la eps
+ * The Certificate class is defined to give the certificates to the users of the eps
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
  * @author:Sofware Bosque S.A
  */
 @Entity
@@ -18,15 +28,27 @@ import lombok.NoArgsConstructor;
 @Data
 public class Certificate {
 /**
- * Se define el paciente con su respectivo dato para poder ingresarlo y mas adelante darle el certificado
+ * The patient is defined with his respective data to be able to enter him and later give him the certificate
  */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "patient", nullable = false)
+    /** 
+    * Private attribute of Patient type with name patient
+    **/
     private Patient patient;
+    /** 
+    * Private attribute of String type with name rute
+    **/
     private String rute;
+    /** 
+    * Private attribute of String type with name type
+    **/
     private String type;
     
 }
