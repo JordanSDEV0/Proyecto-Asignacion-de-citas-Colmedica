1478eeb7caccd2c5d9c47a97b6ca75db7ddd6e51
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-13 22:02:15 -0500

    Update User.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/User.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/User.java
@@ -7,11 +7,18 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Desde esta clase se podra acceder a los user
+ * @author: 
+ *
+ */
 @Entity(name = "eps_user")
 @NoArgsConstructor
 @Data
 @AllArgsConstructor
+/**
+ *Esta clase sera user y estara con atributos de tipo privado con su respectivo Id y password para poder acceder
+ */
 public class User {
 
     @Id
