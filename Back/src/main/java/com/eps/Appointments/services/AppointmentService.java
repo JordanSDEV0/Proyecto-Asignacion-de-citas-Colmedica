b2543e5a0673a64d49b7ec17eb57370ef178b1db
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:35:14 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/services/AppointmentService.java
+++ Back/src/main/java/com/eps/Appointments/services/AppointmentService.java
@@ -1,35 +1,63 @@
+/**
+* Package with which the services of the appointments access
+**/
 package com.eps.Appointments.services;
-
+/**
+* Imports of java util
+*/
 import java.util.List;
-
+/**
+* Imports of springframework
+*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.AppointmentDTO;
 import com.eps.Appointments.mappers.AppointmentMapper;
 import com.eps.Appointments.persistance.entities.Appointment;
 import com.eps.Appointments.persistance.repositories.AppointmentRepository;
-
+/**
+* Creation of the public class AppointmentService
+* @Service It is used to mark the class as a service provider 
+**/
 @Service
 public class AppointmentService{
-
-    @Autowired
+ /**
+    * annotation that allows to inject some dependencies with others inside Spring
+    **/
+    @Autowired 
+    /**
+    * Private attribute of type AppointmentRepository of name appointmentRepository
+    **/
     private AppointmentRepository appointmentRepository;
     @Autowired
+    /**
+    * Private attribute of type AppointmentMapper of name appointmentMapper
+    **/
     private AppointmentMapper appointmentMapper;
-
+    /**
+    * class creation AppointmentDTO with attributes appointmentDTO and exception IllegalArgumentException
+    * @Transactional annotation is the metadata that specifies the semantics of the transactions on a method
+    **/
     @Transactional
     public AppointmentDTO create(AppointmentDTO appointmentDTO) throws IllegalArgumentException{
         System.out.println(appointmentMapper.toAppointment(appointmentDTO));
         return appointmentMapper.toAppointmentDTO(appointmentRepository.save(appointmentMapper.toAppointment(appointmentDTO)));
     }
-    
+    /**
+    * creation of the class List<AppointmentDTO> getAll
+    **/
     public List<AppointmentDTO> getAll(){
         List<Appointment> appointments= (List<Appointment>) appointmentRepository.findAll();
         return appointmentMapper.toAppointmentDTOs(appointments);
     }
-
+    /**
+    * class creation AppointmentDTO updateAppointment with attributes appointmentDTO 
+    * @Transactional annotation is the metadata that specifies the semantics of the transactions on a method
+    **/
     @Transactional
     public AppointmentDTO updateAppointment(AppointmentDTO appointmentDTO){
         if(getById(appointmentDTO.getId()) != null){
@@ -38,7 +66,9 @@ public class AppointmentService{
         }
         return null;
     }
-
+    /**
+    * creation of the class AppointmentDTO getById with id attributes 
+    **/
     public AppointmentDTO getById(int id){
         return appointmentMapper.toAppointmentDTO(appointmentRepository.findById(id).map(ocupation -> {
             return ocupation;
