21986f0e993c6db4c192e7bbd12e104cb6427f74
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:17:05 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/CityDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/CityDTO.java
@@ -1,15 +1,31 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of lombok
+*/
 import lombok.Data;
-
 import lombok.AllArgsConstructor;
-
+/**
+* Public class for dto city
+* @Data allows the use of db data and persistence
+* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
+**/
 @Data
 @AllArgsConstructor
 public class CityDTO {
-
+/**
+    * Private attribute of type int of name id
+    **/
     private int id;
+	/**
+    * Private attribute of type DepartmentDTO of name department
+    **/
 	private DepartmentDTO department;
+	/**
+    * Private attribute of type String of name city
+    **/
     private String city;
 
 }
