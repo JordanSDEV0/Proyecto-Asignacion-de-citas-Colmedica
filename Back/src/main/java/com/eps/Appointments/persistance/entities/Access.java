3cee8973c097e122d0d33ad5b1349a7281d10ed8
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:06:15 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Access.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Access.java
@@ -1,7 +1,14 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports of time
+*/
 import java.time.LocalDateTime;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,27 +16,41 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- * Se tiene la clase Access la cual va a servir para acceder a la eps
- * @author:Sofware Bosque S.A
+ * There is the Access class which will be used to access the eps
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
+ * @author:Software Bosque S.A
  */
 @Entity
 @NoArgsConstructor
 @Data
 public class Access {
 /**
- *Se define los atributos con su respectivo tipo para poder acceder tranquilamente a la eps
+ * The attributes are defined with their respective type to be able to easily access the eps
  */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "eps_user", nullable = false)    
+    /** 
+    * Private attribute of user type with name user
+    **/
     private User user;
     @Column(name = "access_time")
+    /** 
+    * Private attribute of LocalDateTime type with name accessTime
+    **/
     private LocalDateTime accessTime;
 
 }
