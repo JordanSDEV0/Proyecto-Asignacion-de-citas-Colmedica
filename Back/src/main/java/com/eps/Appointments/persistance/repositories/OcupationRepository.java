d395c0fb9d467c9b134aba6d4f7d522e3f8abc69
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:02:28 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/OcupationRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/OcupationRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.Ocupation;
-
+/**
+* interface with name OcupationRepository which comes from CrudRepository<Ocupation, Integer>
+*/
 @Repository
 public interface OcupationRepository extends CrudRepository<Ocupation, Integer>{
 
