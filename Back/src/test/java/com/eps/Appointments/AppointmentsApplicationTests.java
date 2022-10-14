0cd43927e602684c4265dba39e1d193412141f12
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:23:11 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/test/java/com/eps/Appointments/AppointmentsApplicationTests.java
+++ Back/src/test/java/com/eps/Appointments/AppointmentsApplicationTests.java
@@ -1,11 +1,24 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments;
-
+/**
+* Imports org.junit.jupiter
+*/
 import org.junit.jupiter.api.Test;
+/**
+* Imports org.springframework
+*/
 import org.springframework.boot.test.context.SpringBootTest;
-
+/**
+* Class named AppointmentsApplicationTests
+* @SpringBootTest annotation tells Spring Boot to look for a main configuration class
+**/
 @SpringBootTest
 class AppointmentsApplicationTests {
-
+/**
+* Empty contextLoads() is a test to verify if the application is able to load Spring context successfully or not.
+**/
 	@Test
 	void contextLoads() {
 	}
