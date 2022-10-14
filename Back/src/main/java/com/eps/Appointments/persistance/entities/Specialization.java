67a34c7a6a1093c68476c02a6ae56207a3d4fdb7
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:39:12 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Specialization.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Specialization.java
@@ -1,27 +1,42 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.entities;
-
+/**
+* Imports persistence
+*/
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+/**
+* Imports of lombok.data
+*/
 import lombok.Data;
 import lombok.NoArgsConstructor;
 /**
- *
- * @author: 
+ * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @noargsconstructor will generate a constructor with no parameters
+ * @Data allows the use of class data
+ * @author: bdanddoc
  */
 @Entity
 @NoArgsConstructor
 @Data
 /**
- *Esta clase se encarga de manejar la asignacion de especialidad segun el id y la description
+ * This class is in charge of handling the assignment of specialty according to the id and description
  */
 public class Specialization {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /** 
+    * Private attribute of numeric type with name id
+    **/
     private Integer id;
+    /** 
+    * Private attribute of String type with name description
+    **/
     private String description;
     
 }
