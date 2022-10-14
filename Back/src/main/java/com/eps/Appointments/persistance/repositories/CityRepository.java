b280386248c69f83975bf67b9daf597a5fb0844f
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 11:54:11 -0500

    Docuemntación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/CityRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/CityRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.City;
-
+/**
+* interface with name CityRepository which comes from CrudRepository<City, Integer>
+*/
 @Repository
 public interface CityRepository extends CrudRepository<City, Integer> {
 
