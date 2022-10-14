458749a23025c72afdee8a232618f698c2bda684
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:03:20 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/SpecializationRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/SpecializationRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.Specialization;
-
+/**
+* interface with name SpecializationRepository which comes from CrudRepository<Specialization, Integer>
+*/
 @Repository
 public interface SpecializationRepository extends CrudRepository<Specialization, Integer>{
 
