802dcdc2d62f36f0c8e32dd01f8e9c2f05a901d3
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:00:35 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/IdTypeRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/IdTypeRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.IdType;
-
+/**
+* interface with name IdTypeRepository which comes from CrudRepository<IdType, Integer>
+*/
 @Repository
 public interface IdTypeRepository extends CrudRepository<IdType, Integer>{
 
