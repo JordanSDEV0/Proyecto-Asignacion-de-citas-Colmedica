6afe9c5148a08092074babe0ba1819cbd156a91c
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 11:58:28 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/DepartmentRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/DepartmentRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.Department;
-
+/**
+* interface with name DepartmentRepository which comes from CrudRepository<Department, Integer>
+*/
 @Repository
 public interface DepartmentRepository extends CrudRepository<Department, Integer>{
 
