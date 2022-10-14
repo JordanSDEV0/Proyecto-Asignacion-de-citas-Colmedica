a3d5bbf2b930a2fd59127e7f292a2492a796450c
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-13 22:23:40 -0500

    Update Department.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Department.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Department.java
@@ -7,12 +7,17 @@ import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Se crea la clase Departament para saber en donde se encuentra ubicado
+ * @author:Sofware Bosque S.A
+ */
 @Entity
 @NoArgsConstructor
 @Data
 public class Department {
-
+/**
+ * Creamos sus atributos con su respectivo id y department con sus respectivos tipos que son private
+ */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
