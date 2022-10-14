e0fa9dc3f559c0c588f20c0de5e8868abab5c895
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 15:05:51 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/AppointmentDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/AppointmentDTO.java
@@ -1,13 +1,32 @@
+/**
+* Package with which the DTOs of the appointments is accessed
+**/
 package com.eps.Appointments.DTOs;
-
+/**
+* Imports of lombok
+*/
 import lombok.Data;
-
+/**
+* Public class for dto appointment
+* @Data allows the use of db data and persistence
+**/
 @Data
 public class AppointmentDTO {
-
+/**
+    * Private attribute of type int of name id
+    **/
     private int id;
+    /**
+    * Private attribute of type String of name doctorId
+    **/
     private String doctorId;
+    /**
+    * Private attribute of type String of name patientId
+    **/
     private String patientId;
+    /**
+    * Private attribute of type int of name dateId
+    **/
     private int dateId;
 
 }
