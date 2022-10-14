6ef9ee2561c1903bb7d5abcef7e8d08439ec50bb
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 12:04:01 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/persistance/repositories/UserRepository.java
+++ Back/src/main/java/com/eps/Appointments/persistance/repositories/UserRepository.java
@@ -1,10 +1,19 @@
+/**
+* Package with which the persistence of the repositories is accessed
+**/
 package com.eps.Appointments.persistance.repositories;
-
+/**
+* Imports org.springframework
+*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+* Imports com.eps
+*/
 import com.eps.Appointments.persistance.entities.User;
-
+/**
+* interface with name UserRepository which comes from CrudRepository<User, String>
+*/
 @Repository
 public interface UserRepository extends CrudRepository<User, String>{
     
