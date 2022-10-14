61c36b54b56637e85562d76395a11c6ea3370cf8
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:50:36 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/DateMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/DateMapper.java
@@ -1,15 +1,28 @@
+/**
+* Package with which the persistence of the mappers is accessed
+**/
 package com.eps.Appointments.mappers;
+/**
+* Imports of java
+*/
 import java.util.List;
-
+/**
+* Imports of mapstruct
+*/
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.DateDTO;
 import com.eps.Appointments.persistance.entities.Date;
 import com.eps.Appointments.persistance.entities.DateType;
 import com.eps.Appointments.persistance.entities.Headquarter;
-
+/**
+* public name interface DateMapper
+* @mapper geographic information system (GIS)
+**/
 @Mapper(componentModel = "spring", uses = {DateTypeMapper.class, HeadquarterMapper.class})
 public interface DateMapper {
 
