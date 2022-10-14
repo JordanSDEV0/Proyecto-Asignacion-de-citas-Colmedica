ffcd297cb7ce0f6616e609363a13bf634da09d6b
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-10-14 14:55:54 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/mappers/OcupationMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/OcupationMapper.java
@@ -1,14 +1,26 @@
+/**
+* Package with which the persistence of the mappers is accessed
+**/
 package com.eps.Appointments.mappers;
-
+/**
+* Imports of java
+*/
 import java.util.List;
-
+/**
+* Imports of mapstruct
+*/
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.OcupationDTO;
 import com.eps.Appointments.persistance.entities.Ocupation;
-
+/**
+* public name interface OcupationMapper
+* @mapper geographic information system (GIS)
+**/
 @Mapper(componentModel = "spring")
 public interface OcupationMapper {
 
