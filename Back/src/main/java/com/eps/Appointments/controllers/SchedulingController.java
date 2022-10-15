56716e1931cf7712aa5bbccb09112a582fd41a98
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-14 19:43:49 -0500

    Update SchedulingController.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/controllers/SchedulingController.java
+++ Back/src/main/java/com/eps/Appointments/controllers/SchedulingController.java
@@ -2,8 +2,9 @@
 * paquete por donde se accede al controlador
 **/
 package com.eps.Appointments.controllers;
-
-
+/**
+*  imports of Springframework
+**/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +15,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.SchedulingDTO;
 import com.eps.Appointments.DTOs.ErrorDTO;
 import com.eps.Appointments.services.SchedulingService;
-
+/**
+* @RestController Simplifica la implementacion del controller
+* @RequestMapping para asignar todas las URL de solicitudes HTTP entrantes a los métodos de controlador correspondientes
+* @CrossOrigin permitir solicitudes de origen cruzado en clases de controlador específicas y/o métodos de controlador
+**/
 @RestController
 @RequestMapping("/scheduling")
 @CrossOrigin
 public class SchedulingController {
 
+/**
+* Se crea una clase publica llamada shedulingController
+**/	
     @Autowired
     private SchedulingService schedulingService;
+/**
+* Se crea un metodo privado llamada shedulingservice 
+* @Autowired. Permite que Spring resuelva e inyecte beans colaboradores en nuestro bean
+**/
 
+	
+/**
+* Se crea un metodo llamada ResponseEntity  en donde dara la orden de mostrarnos si sale si la cita no fue creada 
+* @PostMapping es una versión especializada de la anotación @RequestMapping
+**/
     @PostMapping
     private ResponseEntity<? extends Object> create(@RequestBody SchedulingDTO scheduling){
         try {
@@ -43,7 +62,10 @@ public class SchedulingController {
             return new ResponseEntity<ErrorDTO>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
-
+/**
+*Se crea un metodo tipo publico el cual obtendra esta informacion
+*@GetMapping es una anotación compuesta que actúa como acceso directo para @RequestMapping
+**/
     @GetMapping
     public ResponseEntity<? extends Object> getAll(){
         try{
@@ -56,7 +78,10 @@ public class SchedulingController {
             return new ResponseEntity<>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
-
+/**
+* se crea un metodo de tipo publica el cual tendra la funcion de obtener la informacion
+* @GetMapping es una anotación compuesta que actúa como acceso directo para @RequestMapping
+**/
     @GetMapping("/{id}")
         public ResponseEntity<? extends Object> getById(@PathVariable("id") Integer id){
         try{
