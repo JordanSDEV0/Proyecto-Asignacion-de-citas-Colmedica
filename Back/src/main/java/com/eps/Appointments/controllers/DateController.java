5a0d7d3bd160858403f069c14ec795e5681e2fa5
jtobarg <106928822+jtobarg@users.noreply.github.com>
2022-10-14 22:31:45 -0500

    Update DateController.java
    Signed-off-by: jtobarg <106928822+jtobarg@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/controllers/DateController.java
+++ Back/src/main/java/com/eps/Appointments/controllers/DateController.java
@@ -1,5 +1,11 @@
-package com.eps.Appointments.controllers;
+/**
+* paquete por donde se accede al controlador
+**/
 
+package com.eps.Appointments.controllers;
+/**
+*  imports of Springframework
+**/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,19 +17,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+* Imports of Appointments
+*/
 import com.eps.Appointments.DTOs.DateDTO;
 import com.eps.Appointments.DTOs.ErrorDTO;
 import com.eps.Appointments.services.DateService;
-
+/**
+* @RestController Simplifica la implementacion del controller
+* @RequestMapping para asignar todas las URL de solicitudes HTTP entrantes a los métodos de controlador correspondientes
+*@CrossOrigin permitir solicitudes de origen cruzado en clases de controlador específicas y/o métodos de controlador
+**/
 @RestController
 @RequestMapping("/dates")
 @CrossOrigin(origins = "*")
-public class DateController {
 
+public class DateController {
+/**
+*Se crea una clase DateController
+* @Autowired. Permite que Spring resuelva e inyecte beans colaboradores en nuestro bean
+**/
     @Autowired
     private DateService dateService;
-
+/**
+* Se crea un metodo el cual sirve para mostrar si el dato fue creado
+* @PostMapping es una versión especializada de la anotación @RequestMapping
+**/
     @PostMapping
     private ResponseEntity<? extends Object> create(@RequestBody DateDTO dateDTO){
         try{
@@ -40,7 +59,10 @@ public class DateController {
             return new ResponseEntity<ErrorDTO>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
-
+/**
+* Se crea un metodo el  cual pueda obtener esta informacion y diga si el admin puede o no por medio de id
+* @GetMapping es una anotación compuesta que actúa como acceso directo para @RequestMapping
+**/
     @GetMapping("/{id}")
     public ResponseEntity<? extends Object> getById(@PathVariable("id") Integer id){
         try{
@@ -57,7 +79,10 @@ public class DateController {
             return new ResponseEntity<ErrorDTO>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
-
+/**
+* se crea un metodo para que pueda comprobar si la informacion se esta obteniendo
+* @GetMapping es una anotación compuesta que actúa como acceso directo para @RequestMapping
+**/
     @GetMapping
     public ResponseEntity<? extends Object> getAll(){
         try{
@@ -70,7 +95,10 @@ public class DateController {
             return new ResponseEntity<>(new ErrorDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
-
+/**
+* se hace un solicitud y valida si el paciente no esta creado
+* @PutMapping en su aplicación de servicios web RESTful para poder aceptar solicitudes HTTP Put que contengan un cuerpo de solicitud con JSON
+**/
     @PutMapping("/{id}")
     private ResponseEntity<? extends Object> Update(@RequestBody DateDTO dateDTO, @PathVariable("id") String id){
         try {
