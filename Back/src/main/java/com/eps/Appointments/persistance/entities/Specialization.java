9e9a502a9433fbece722d795ca3c347a167451c8
William Fonseca <santifon15@gmail.com>
2022-11-12 18:27:21 -0500

    refactor specialization

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Specialization.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Specialization.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports persistence
 */
@@ -14,8 +15,10 @@ import javax.persistence.Id;
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
  * @author: bdanddoc
@@ -24,19 +27,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 /**
- * This class is in charge of handling the assignment of specialty according to the id and description
+ * This class is in charge of handling the assignment of specialty according to
+ * the id and description
  */
 public class Specialization {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    /** 
-    * Private attribute of numeric type with name id
-    **/
+    /**
+     * Private attribute of numeric type with name id
+     **/
     private Integer id;
-    /** 
-    * Private attribute of String type with name description
-    **/
+    /**
+     * Private attribute of String type with name description
+     **/
     private String description;
-    
+
 }
