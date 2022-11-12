d34a676596413d9445854610eeb22cb15599827b
William Fonseca <santifon15@gmail.com>
2022-11-12 18:08:08 -0500

    refactor access

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Access.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Access.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports of time
 */
@@ -16,14 +17,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
 * Imports of lombok.data
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * There is the Access class which will be used to access the eps
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * 
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
  * @author:Software Bosque S.A
@@ -32,25 +39,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Access {
-/**
- * The attributes are defined with their respective type to be able to easily access the eps
- */
+    /**
+     * The attributes are defined with their respective type to be able to easily
+     * access the eps
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    /** 
-    * Private attribute of numeric type with name id
-    **/
+    /**
+     * Private attribute of numeric type with name id
+     **/
     private Integer id;
     @ManyToOne
-    @JoinColumn(name = "eps_user", nullable = false)    
-    /** 
-    * Private attribute of user type with name user
-    **/
+    @JoinColumn(name = "eps_user", nullable = false)
+    /**
+     * Private attribute of user type with name user
+     **/
     private User user;
     @Column(name = "access_time")
-    /** 
-    * Private attribute of LocalDateTime type with name accessTime
-    **/
-    private LocalDateTime accessTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    /**
+     * Private attribute of LocalDateTime type with name accessTime
+     **/
+    private Date accessTime;
 
 }
