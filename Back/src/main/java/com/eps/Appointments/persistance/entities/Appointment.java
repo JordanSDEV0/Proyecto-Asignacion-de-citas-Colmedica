8322cac69e897278ab17e0dde5b488fd0beaf6d1
William Fonseca <santifon15@gmail.com>
2022-11-12 18:10:07 -0500

    refactor appointment

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Appointment.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Appointment.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports persistence
 */
@@ -16,9 +17,12 @@ import javax.persistence.ManyToOne;
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
- * Se tiene la clase Appointment la cual son citas 
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * Se tiene la clase Appointment la cual son citas
+ *
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
  * @author:Sofware Bosque S.A
@@ -27,32 +31,33 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Appointment {
-/**
- * This class is defined by the doctor, patient and data to be able to know everything about the appointment
- */
+    /**
+     * This class is defined by the doctor, patient and data to be able to know
+     * everything about the appointment
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     /**
-    * Private attribute of numeric type with name id
-    **/
+     * Private attribute of numeric type with name id
+     **/
     private Integer id;
     @ManyToOne
     @JoinColumn(nullable = false)
     /**
-    * Private attribute of doctor type with name Doctor
-    **/
+     * Private attribute of doctor type with name Doctor
+     **/
     private Doctor doctor;
     @ManyToOne
     @JoinColumn(nullable = false)
     /**
-    * Private attribute of Patient type with name patient
-    **/
+     * Private attribute of Patient type with name patient
+     **/
     private Patient patient;
     @ManyToOne
     @JoinColumn(nullable = false)
     /**
-    * Private attribute of Date type with name date
-    **/
+     * Private attribute of Date type with name date
+     **/
     private Date date;
 
 }
