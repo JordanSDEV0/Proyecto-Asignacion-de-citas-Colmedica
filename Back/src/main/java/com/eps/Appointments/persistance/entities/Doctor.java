4fd0a7e3a86816abc086b9b96713531a0bb05b3a
William Fonseca <santifon15@gmail.com>
2022-11-12 18:18:57 -0500

    refactor doctor

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Doctor.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Doctor.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports of java
 */
@@ -25,51 +26,53 @@ import javax.persistence.OneToOne;
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * The doctor class is created to define its role in the eps
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ * 
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
- * @author:Sofware Bosque S.A
-  */
+ * @author:Software Bosque S.A
+ */
 @Entity
 @NoArgsConstructor
 @Data
-public class Doctor implements Serializable{
-/**
- * It has its attributes to know the doctor in which specialty he is and in which center he is attending
- */
-    @Id
-    @Column(name = "eps_user")
-   /** 
-    * Private attribute of String type with name id
-    **/
-    private String id;
-    @OneToOne(cascade = CascadeType.ALL)
-    @MapsId
-    @JoinColumn(name = "eps_user")
-    /** 
-    * Private attribute of User type with name user
-    **/
-    private User user;
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    /** 
-    * Private attribute of Headquarter type with name headquarter
-    **/
-    private Headquarter headquarter;
-    @ManyToMany
-    @JoinTable(name = "doctor_specialization")
-    /** 
-    * Private attribute of list Specialization type with name specialization
-    **/
-    private List<Specialization> specialization;
+public class Doctor implements Serializable {
+  /**
+   * It has its attributes to know the doctor in which specialty he is and in
+   * which center he is attending
+   */
+  @Id
+  @Column(name = "eps_user")
+  /**
+   * Private attribute of String type with name id
+   **/
+  private String id;
+  @OneToOne(cascade = CascadeType.ALL)
+  @MapsId
+  @JoinColumn(name = "eps_user")
+  /**
+   * Private attribute of User type with name user
+   **/
+  private User user;
+  @ManyToOne
+  @JoinColumn(nullable = false)
+  /**
+   * Private attribute of Headquarter type with name headquarter
+   **/
+  private Headquarter headquarter;
+  @ManyToMany
+  @JoinTable(name = "doctor_specialization")
+  /**
+   * Private attribute of list Specialization type with name specialization
+   **/
+  private List<Specialization> specialization;
+  private String name;
+  @Column(name = "last_name")
+  private String lastName;
+  private int phone;
+  private String email;
 
-    private String name_doctor;
-    @Column(name = "last_name")
-
-    private String lastName;
-    private String email;
-    private int phone;
-    
 }
