5ba2539b538fdf9c7a94f235e81cda76e2648c65
William Fonseca <santifon15@gmail.com>
2022-11-12 18:15:38 -0500

    refactor date type

--- Back/src/main/java/com/eps/Appointments/persistance/entities/DateType.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/DateType.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports persistence
 */
@@ -14,29 +15,32 @@ import javax.persistence.Id;
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * DateType class is created
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ *
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
- * @Data allows the use of class data 
- * @author:Sofware Bosque S.A
+ * @Data allows the use of class data
+ * @author:Software Bosque S.A
  */
 @Entity(name = "date_type")
 @NoArgsConstructor
 @Data
 public class DateType {
-/**
- * Attributes are generated as id and description with their respective type
- */
+    /**
+     * Attributes are generated as id and description with their respective type
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-     /** 
-    * Private attribute of numeric type with name id
-    **/
+    /**
+     * Private attribute of numeric type with name id
+     **/
     private Integer id;
-     /** 
-    * Private attribute of numeric type with name id
-    **/
+    /**
+     * Private attribute of numeric type with name id
+     **/
     private String description;
-    
+
 }
