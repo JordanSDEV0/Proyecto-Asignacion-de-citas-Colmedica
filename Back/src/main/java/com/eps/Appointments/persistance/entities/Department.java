3c44289d611f7620bba7e78bc9651ab0137e98c7
William Fonseca <santifon15@gmail.com>
2022-11-12 18:16:21 -0500

    refactor department

--- Back/src/main/java/com/eps/Appointments/persistance/entities/Department.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/Department.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports persistence
 */
@@ -14,29 +15,33 @@ import javax.persistence.Id;
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * The Department class is created to know where it is located
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ *
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
- * @author:Sofware Bosque S.A
+ * @author:Software Bosque S.A
  */
 @Entity
 @NoArgsConstructor
 @Data
 public class Department {
-/**
- * We create its attributes with their respective id and department with their respective types that are private
- */
+    /**
+     * We create its attributes with their respective id and department with their
+     * respective types that are private
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    /** 
-    * Private attribute of numeric type with name id
-    **/
+    /**
+     * Private attribute of numeric type with name id
+     **/
     private Integer id;
-    /** 
-    * Private attribute of String type with name departament
-    **/
+    /**
+     * Private attribute of String type with name department
+     **/
     private String department;
-    
+
 }
