df0434d75429354baa529bf07977dce07f12b28d
William Fonseca <santifon15@gmail.com>
2022-11-12 18:03:00 -0500

    refactor User

--- Back/src/main/java/com/eps/Appointments/persistance/entities/User.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/User.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports persistence
 */
@@ -14,9 +15,12 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * From this class you can access the user
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ *
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
  * @author:Software Bosque S.A
@@ -27,23 +31,21 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 /**
- * This class will be user and will have attributes of a private type with its respective Id and password to be able to access
+ * This class will be user and will have attributes of a private type with its
+ * respective Id and password to be able to access
  */
 public class User {
 
-    public User(String id2, String password2) {
-    }
     @Id
-    /** 
-    * Private attribute of String type with name id
-    **/
+    /**
+     * Private attribute of String type with name id
+     **/
     private String id;
     @Column(nullable = false)
-    /** 
-    * Private attribute of String type with name password
-    **/
+    /**
+     * Private attribute of String type with name password
+     **/
     private String password;
     private int rol;
 
-    
 }
