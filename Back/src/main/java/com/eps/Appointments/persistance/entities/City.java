409951032102c98aebfd1e2ce8ba9f06fa76d05a
William Fonseca <santifon15@gmail.com>
2022-11-12 18:12:17 -0500

    refactor city

--- Back/src/main/java/com/eps/Appointments/persistance/entities/City.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/City.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports persistence
 */
@@ -16,35 +17,39 @@ import javax.persistence.ManyToOne;
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * The city class is defined to know the locations of the centers
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ *
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
- * @author:Sofware Bosque S.A
+ * @author:Software Bosque S.A
  */
 @Entity
 @NoArgsConstructor
 @Data
 public class City {
-/**
- * The cities are defined with their department id and city, this means that it will show us the department and city where the center is.
- */
+    /**
+     * The cities are defined with their department id and city, this means that it
+     * will show us the department and city where the center is.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-     /** 
-    * Private attribute of numeric type with name id
-    **/
+    /**
+     * Private attribute of numeric type with name id
+     **/
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "department", nullable = false)
-     /** 
-    * Private attribute of Department type with name department
-    **/
+    /**
+     * Private attribute of Department type with name department
+     **/
     private Department department;
-     /** 
-    * Private attribute of String type with name city
-    **/
+    /**
+     * Private attribute of String type with name city
+     **/
     private String city;
-    
+
 }
