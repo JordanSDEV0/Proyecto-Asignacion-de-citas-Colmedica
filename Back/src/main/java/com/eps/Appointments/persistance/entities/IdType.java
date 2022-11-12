e956edcdfe645aa3997791ea13f13f5fd197fa55
William Fonseca <santifon15@gmail.com>
2022-11-12 18:20:35 -0500

    refactor id type

--- Back/src/main/java/com/eps/Appointments/persistance/entities/IdType.java
+++ Back/src/main/java/com/eps/Appointments/persistance/entities/IdType.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the repositories is accessed
 **/
 package com.eps.Appointments.persistance.entities;
+
 /**
 * Imports persistence
 */
@@ -14,29 +15,33 @@ import javax.persistence.Id;
 */
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * IdType class is created to hold each type of ID
- * @entity The Entity Framework allows developers to work with data in the form of specific objects and properties
+ *
+ * @entity The Entity Framework allows developers to work with data in the form
+ *         of specific objects and properties
  * @noargsconstructor will generate a constructor with no parameters
  * @Data allows the use of class data
- * @author:Sofware Bosque S.A
+ * @author:Software Bosque S.A
  */
 @Entity
 @NoArgsConstructor
 @Data
 public class IdType {
-/**
- * Its attributes are defined, which are id and type, with their respective types, which are private.
- */
+    /**
+     * Its attributes are defined, which are id and type, with their respective
+     * types, which are private.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    /** 
-    * Private attribute of numeric type with name id
-    **/
+    /**
+     * Private attribute of numeric type with name id
+     **/
     private Integer id;
-    /** 
-    * Private attribute of String type with name type
-    **/
+    /**
+     * Private attribute of String type with name type
+     **/
     private String type;
-    
+
 }
