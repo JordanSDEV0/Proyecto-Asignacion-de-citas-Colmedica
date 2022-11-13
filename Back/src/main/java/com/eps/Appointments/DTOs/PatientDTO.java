df4250e109e6338edc35959fa56414ec359c66e5
William Fonseca <santifon15@gmail.com>
2022-11-12 19:52:03 -0500

    refactor patient dto

--- Back/src/main/java/com/eps/Appointments/DTOs/PatientDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/PatientDTO.java
@@ -2,6 +2,7 @@
 * Package with which the DTOs of the appointments is accessed
 **/
 package com.eps.Appointments.DTOs;
+
 /**
 * Imports of time
 */
@@ -11,67 +12,69 @@ import java.time.LocalDate;
 */
 import lombok.Data;
 import lombok.AllArgsConstructor;
+
 /**
-* Public class for dto patient
-* @Data allows the use of db data and persistence
-* @AllArgsConstructor annotation automatically generates a constructor with a parameter for each field in your class
-**/
+ * Public class for dto patient
+ * 
+ * @Data allows the use of db data and persistence
+ * @AllArgsConstructor annotation automatically generates a constructor with a
+ *                     parameter for each field in your class
+ **/
 @Data
 @AllArgsConstructor
 public class PatientDTO {
     /**
-    * Private attribute of type String of name id
-    **/
+     * Private attribute of type String of name id
+     **/
     private String id;
     /**
-    * Private attribute of type String of name password
-    **/
+     * Private attribute of type String of name password
+     **/
     private String password;
-    private int rol;
 
     /**
-    * Private attribute of type int of name idTypeId
-    **/
+     * Private attribute of type int of name idTypeId
+     **/
     private int idTypeId;
     /**
-    * Private attribute of type int of name headquarterId
-    **/
+     * Private attribute of type int of name headquarterId
+     **/
     private int headquarterId;
     /**
-    * Private attribute of type int of name ocupationId
-    **/
+     * Private attribute of type int of name ocupationId
+     **/
     private int ocupationId;
     /**
-    * Private attribute of type String of name regimenType
-    **/
+     * Private attribute of type String of name regimenType
+     **/
     private String regimenType;
     /**
-    * Private attribute of type String of name phone
-    **/
+     * Private attribute of type String of name phone
+     **/
     private String phone;
     /**
-    * Private attribute of type String of name name
-    **/
+     * Private attribute of type String of name name
+     **/
     private String name;
     /**
-    * Private attribute of type String of name lastName
-    **/
+     * Private attribute of type String of name lastName
+     **/
     private String lastName;
     /**
-    * Private attribute of type String of name address
-    **/
+     * Private attribute of type String of name address
+     **/
     private String address;
     /**
-    * Private attribute of type String of name email
-    **/
+     * Private attribute of type String of name email
+     **/
     private String email;
     /**
-    * Private attribute of type LocalDate of name birthDate
-    **/
+     * Private attribute of type LocalDate of name birthDate
+     **/
     private LocalDate birthDate;
     /**
-    * Private attribute of type String of name genre
-    **/
+     * Private attribute of type String of name genre
+     **/
     private String genre;
 
 }
