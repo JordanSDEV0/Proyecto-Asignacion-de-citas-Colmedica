eedf3d1d323f01df7f1178630ad51fb5ccb8bd49
William Fonseca <santifon15@gmail.com>
2022-11-12 19:41:12 -0500

    add user details service and user details impl

--- Back/src/main/java/com/eps/Appointments/configuration/SecurityConfig.java
+++ Back/src/main/java/com/eps/Appointments/configuration/SecurityConfig.java
@@ -6,17 +6,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.eps.Appointments.Security.JpaUserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	private JpaUserDetailsService jpaUserDetailsService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 				.csrf(csrf -> csrf.disable())
-				.authorizeRequests(auth -> {
-					auth.antMatchers().permitAll();
-				})
+				.userDetailsService(jpaUserDetailsService)
 				.build();
 	}
 
