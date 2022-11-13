c6a08fdd13742383f4545df2208b9afa1d663ff2
William Fonseca <santifon15@gmail.com>
2022-11-12 20:13:48 -0500

    remove user  mapper

--- Back/src/main/java/com/eps/Appointments/mappers/UserMapper.java
+++ Back/src/main/java/com/eps/Appointments/mappers/UserMapper.java
@@ -2,6 +2,7 @@
 * Package with which the persistence of the mappers is accessed
 **/
 package com.eps.Appointments.mappers;
+
 /**
 * Imports of java
 */
@@ -17,24 +18,28 @@ import org.mapstruct.Mapping;
 */
 import com.eps.Appointments.DTOs.UserDTO;
 import com.eps.Appointments.persistance.entities.User;
+
 /**
-* public name interface UserMapper
-* @mapper geographic information system (GIS)
-**/
-@Mapper(componentModel = "spring")
+ * public name interface UserMapper
+ *
+ * @mapper geographic information system (GIS)
+ **/
+// @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     @Mapping(source = "id", target = "id")
     @Mapping(source = "password", target = "password")
     @Mapping(source = "rol", target = "rol")
 
-    UserDTO toUserDTO (User user);
-/**
-* Advises the code generator to apply all the Mapping s from an inverse mapping method to the annotated method as well
-**/
+    UserDTO toUserDTO(User user);
+
+    /**
+     * Advises the code generator to apply all the Mapping s from an inverse mapping
+     * method to the annotated method as well
+     **/
     @InheritInverseConfiguration
-    User toUser (UserDTO userDTO);
+    User toUser(UserDTO userDTO);
 
     List<UserDTO> toUserDTOs(List<User> user);
-     
+
 }
