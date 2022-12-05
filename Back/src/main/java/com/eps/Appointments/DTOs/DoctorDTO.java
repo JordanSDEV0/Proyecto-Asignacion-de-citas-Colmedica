a78b4267524d7c1efd49a90c5aeb16dc23c76aa9
PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>
2022-12-05 08:23:07 -0500

    Documentación
    Signed-off-by: PaulaBuitrago <65428260+PaulaBuitragp@users.noreply.github.com>

--- Back/src/main/java/com/eps/Appointments/DTOs/DoctorDTO.java
+++ Back/src/main/java/com/eps/Appointments/DTOs/DoctorDTO.java
@@ -27,15 +27,30 @@ public class DoctorDTO {
     * Private attribute of type String of name password
     **/
     private String password;
+    /**
+    * Private attribute of type int of name rol
+    **/
     private int rol;
 
     /**
     * Private attribute of type int of name headquarterId
     **/
     private int headquarterId;
+    /**
+    * Private attribute of type string of name name doctor
+    **/
     private String name_doctor;
+    /**
+    * Private attribute of type string of name last name
+    **/
     private String lastName;
+    /**
+    * Private attribute of type string of name email
+    **/
     private String email;
+    /**
+    * Private attribute of type int of name phone
+    **/
     private int phone;
     /**
     * Private attribute of type list SpecializationDTO of name specializationList
